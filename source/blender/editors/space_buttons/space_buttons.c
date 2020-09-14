360549b31bc91f107c1a70dba3e68815e6f39b8c
Hans Goudey <h.goudey@me.com>
2020-09-14 10:39:14 -0500

    Property Search: Use pointer for properties space runtime struct

--- source/blender/editors/space_buttons/space_buttons.c
+++ source/blender/editors/space_buttons/space_buttons.c
@@ -109,11 +109,17 @@ static void buttons_free(SpaceLink *sl)
     BLI_freelistN(&ct->users);
     MEM_freeN(ct);
   }
+
+  BLI_assert(sbuts->runtime != NULL);
+  MEM_freeN(sbuts->runtime);
 }
 
 /* spacetype; init callback */
-static void buttons_init(struct wmWindowManager *UNUSED(wm), ScrArea *UNUSED(area))
+static void buttons_init(struct wmWindowManager *UNUSED(wm), ScrArea *area)
 {
+  SpaceProperties *sbuts = (SpaceProperties *)area->spacedata.first;
+
+  sbuts->runtime = MEM_mallocN(sizeof(SpaceProperties_Runtime), __func__);
 }
 
 static SpaceLink *buttons_duplicate(SpaceLink *sl)
@@ -123,7 +129,7 @@ static SpaceLink *buttons_duplicate(SpaceLink *sl)
   /* clear or remove stuff from old */
   sbutsn->path = NULL;
   sbutsn->texuser = NULL;
-  sbutsn->runtime.search_string[0] = '\0';
+  sbutsn->runtime->search_string[0] = '\0';
 
   return (SpaceLink *)sbutsn;
 }
