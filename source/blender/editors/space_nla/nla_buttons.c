a30c8168a0181e3a267c21270f21b03ab4048548
Wayde Moss <wbmoss_dev@yahoo.com>
2020-11-29 15:14:09 -0500

    - due to last commit, no longer need C-side python UI for alignment properties

--- source/blender/editors/space_nla/nla_buttons.c
+++ source/blender/editors/space_nla/nla_buttons.c
@@ -415,25 +415,6 @@ static void nla_panel_properties(const bContext *C, Panel *panel)
     uiItemR(row, &strip_ptr, "use_reverse", 0, NULL, ICON_NONE);
 
     uiItemR(column, &strip_ptr, "use_animated_time_cyclic", 0, NULL, ICON_NONE);
-
-    /** GG: I don't know why, but implementing UI in python results in wrong rna path (rooting from
-     * RNA Scene type) for blend xform properties and updates don't work properly.
-     *
-     * Here, i guess since we specify the pointerRNA as a strip, it will be correct?
-     */
-    NlaStrip *strip = ((NlaStrip *)strip_ptr.data);
-
-    // temporary: only show 1st one for testing purposes, so when xform changes, it updates nla
-    if (strip->blend_transforms.first) {
-
-      PointerRNA blend_rna;
-      RNA_pointer_create(
-          strip_ptr.owner_id, &RNA_NlaBlendTransform, strip->blend_transforms.first, &blend_rna);
-
-      uiItemR(column, &blend_rna, "location", 0, NULL, ICON_NONE);
-      uiItemR(column, &blend_rna, "euler", 0, NULL, ICON_NONE);
-      uiItemR(column, &blend_rna, "scale", 0, NULL, ICON_NONE);
-    }
   }
 }
 
