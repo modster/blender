ad96f6b8c6ab62cdc730826ff26a3c1788fe9ac5
Julian Eisel <julian@blender.org>
2020-12-03 20:54:12 +0100

    General cleanup of the "Make Asset" oeprator

--- source/blender/editors/asset/asset_ops.c
+++ source/blender/editors/asset/asset_ops.c
@@ -44,7 +44,7 @@
 /**
  * Return the IDs to operate on as list of #CollectionPointerLink links. Needs freeing.
  */
-static ListBase asset_make_get_ids_from_context(const bContext *C)
+static ListBase /* CollectionPointerLink */ asset_make_get_ids_from_context(const bContext *C)
 {
   ListBase list = {0};
 
@@ -64,20 +64,18 @@ static ListBase asset_make_get_ids_from_context(const bContext *C)
 
 struct AssetMakeResultStats {
   int tot_created;
-  int tot_selected_ids;
   int tot_already_asset;
   ID *last_id;
 };
 
 static void asset_make_for_idptr_list(const bContext *C,
-                                      const ListBase *ids,
+                                      const ListBase /* CollectionPointerLink */ *ids,
                                       struct AssetMakeResultStats *r_stats)
 {
   memset(r_stats, 0, sizeof(*r_stats));
 
   LISTBASE_FOREACH (CollectionPointerLink *, ctx_id, ids) {
     BLI_assert(RNA_struct_is_ID(ctx_id->ptr.type));
-    r_stats->tot_selected_ids++;
 
     ID *id = ctx_id->ptr.data;
     if (id->asset_data) {
@@ -94,6 +92,35 @@ static void asset_make_for_idptr_list(const bContext *C,
   }
 }
 
+static bool asset_make_results_report(const struct AssetMakeResultStats *stats,
+                                      ReportList *reports)
+{
+  /* User feedback on failure. */
+  if ((stats->tot_created < 1) && (stats->tot_already_asset > 0)) {
+    BKE_report(reports,
+               RPT_ERROR,
+               "Selected data-blocks are already assets (or do not support use as assets)");
+    return false;
+  }
+  if (stats->tot_created < 1) {
+    BKE_report(reports,
+               RPT_ERROR,
+               "No data-blocks to create assets for found (or do not support use as assets)");
+    return false;
+  }
+
+  /* User feedback on success. */
+  if (stats->tot_created == 1) {
+    /* If only one data-block: Give more useful message by printing asset name. */
+    BKE_reportf(reports, RPT_INFO, "Data-block '%s' is now an asset", stats->last_id->name + 2);
+  }
+  else {
+    BKE_reportf(reports, RPT_INFO, "%i data-blocks are now assets", stats->tot_created);
+  }
+
+  return true;
+}
+
 static int asset_make_exec(bContext *C, wmOperator *op)
 {
   ListBase ids = asset_make_get_ids_from_context(C);
@@ -102,23 +129,8 @@ static int asset_make_exec(bContext *C, wmOperator *op)
   asset_make_for_idptr_list(C, &ids, &stats);
   BLI_freelistN(&ids);
 
-  /* User feedback on failure. */
-  if ((stats.tot_created < 1) && (stats.tot_already_asset > 0)) {
-    BKE_report(op->reports, RPT_ERROR, "Selected data-blocks are already assets");
+  if (!asset_make_results_report(&stats, op->reports)) {
     return OPERATOR_CANCELLED;
-  }
-  if (stats.tot_created < 1) {
-    BKE_report(op->reports, RPT_ERROR, "No data-blocks to create assets for found");
-    return OPERATOR_CANCELLED;
-  }
-
-  /* User feedback on success. */
-  if (stats.tot_created == 1) {
-    /* If only one data-block: Give more useful message by printing asset name. */
-    BKE_reportf(op->reports, RPT_INFO, "Data-block '%s' is now an asset", stats.last_id->name + 2);
-  }
-  else {
-    BKE_reportf(op->reports, RPT_INFO, "%i data-blocks are now assets", stats.tot_created);
   }
 
   WM_main_add_notifier(NC_ID | NA_EDITED, NULL);
