61e5c665b7f1f90ec4eb40cb9e6a84fdd8441113
Clément Foucault <foucault.clem@gmail.com>
2021-01-27 17:11:02 +0100

    EEVEE: Depth Of Field: Fix fast gather tiles bleeding color
    Seems like the dilation is too small for fast gathering to not bleed.
    Introduce an arbitrary factor to fix this issue.

--- source/blender/draw/engines/eevee/eevee_depth_of_field.c
+++ source/blender/draw/engines/eevee/eevee_depth_of_field.c
@@ -150,6 +150,7 @@ int EEVEE_depth_of_field_init(EEVEE_ViewLayerData *UNUSED(sldata),
 #define TILE_DIVISOR 16
 #define GATHER_RING_COUNT 3
 #define DILATE_RING_COUNT 3
+#define FAST_GATHER_COC_ERROR 0.05
 
 /**
  * Create bokeh texture.
@@ -301,9 +302,14 @@ static void dof_dilate_tiles_pass_draw(EEVEE_FramebufferList *fbl,
   for (int pass = 0; pass < 2; pass++) {
     DRWPass *drw_pass = (pass == 0) ? psl->dof_dilate_tiles_minmax : psl->dof_dilate_tiles_minabs;
 
+    /* Not in reference implementation. Strange we need to have something like this.
+     * Factor 4 is arbitrary and was chosen to fix a specific scene. It might not work for
+     * every scene. */
+    const float fast_gather_error = 1.0f / (1.0f - FAST_GATHER_COC_ERROR * 4.0f);
     /* Error introduced by gather center jittering. */
     const float error_multiplier = 1.0f + 1.0f / (GATHER_RING_COUNT + 0.5f);
-    int dilation_end_radius = ceilf((fx->dof_fx_max_coc * error_multiplier) / TILE_DIVISOR);
+    int dilation_end_radius = ceilf((fx->dof_fx_max_coc * error_multiplier * fast_gather_error) /
+                                    TILE_DIVISOR);
 
     /* This algorithm produce the exact dilation radius by dividing it in multiple passes. */
     int dilation_radius = 0;
