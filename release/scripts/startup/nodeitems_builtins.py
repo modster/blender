caac062b55c8d82da1ac8fa975745ea19c26d50b
Fabian Schempp <fabianschempp@googlemail.com>
2021-05-20 10:21:14 +0200

    Initial setup for solidify node

--- release/scripts/startup/nodeitems_builtins.py
+++ release/scripts/startup/nodeitems_builtins.py
@@ -534,6 +534,7 @@ geometry_node_categories = [
         NodeItem("GeometryNodeEdgeSplit"),
         NodeItem("GeometryNodeSubdivisionSurface"),
         NodeItem("GeometryNodeSubdivide"),
+        NodeItem("GeometryNodeSolidify"),
     ]),
     GeometryNodeCategory("GEO_PRIMITIVES", "Mesh Primitives", items=[
         NodeItem("GeometryNodeMeshCircle"),
