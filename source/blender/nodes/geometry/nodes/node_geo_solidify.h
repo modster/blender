a58a4cfc146dcb2ad209cabc962244717ec62866
Fabian Schempp <fabianschempp@googlemail.com>
2021-05-24 18:56:54 +0200

    - working with data from node

--- source/blender/nodes/geometry/nodes/node_geo_solidify.h
+++ source/blender/nodes/geometry/nodes/node_geo_solidify.h
@@ -6,7 +6,41 @@
 extern "C" {
 #endif
 
-Mesh *solidify_extrude_modifyMesh(Mesh *mesh);
+typedef struct SolidifyNodeData {
+  /** Name of vertex group to use, MAX_VGROUP_NAME. */
+  char defgrp_name[64];
+  char shell_defgrp_name[64];
+  char rim_defgrp_name[64];
+  /** New surface offset level. */
+  float offset;
+  /** Midpoint of the offset. */
+  float offset_fac;
+  /**
+   * Factor for the minimum weight to use when vertex-groups are used,
+   * avoids 0.0 weights giving duplicate geometry.
+   */
+  float offset_fac_vg;
+  /** Clamp offset based on surrounding geometry. */
+  float offset_clamp;
+  char mode;
+
+  /** Variables for #MOD_SOLIDIFY_MODE_NONMANIFOLD. */
+  char nonmanifold_offset_mode;
+  char nonmanifold_boundary_mode;
+
+  char _pad;
+  float crease_inner;
+  float crease_outer;
+  float crease_rim;
+  int flag;
+  short mat_ofs;
+  short mat_ofs_rim;
+
+  float merge_tolerance;
+  float bevel_convex;
+} SolidifyNodeData;
+
+Mesh *solidify_extrude_modifyMesh(SolidifyNodeData *smd, Mesh *mesh);
 
 #ifdef __cplusplus
 }
