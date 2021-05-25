cb9504187acd7a7f1ec5fa09091f66ce8f821f7f
YimingWu <xp8110@outlook.com>
2021-05-25 13:02:16 +0800

    LineArt: Change chain_smooth_tolerance default to 0.

--- source/blender/makesdna/DNA_gpencil_modifier_defaults.h
+++ source/blender/makesdna/DNA_gpencil_modifier_defaults.h
@@ -298,7 +298,7 @@
     .angle_splitting_threshold = DEG2RAD(60.0f), \
     .chaining_image_threshold = 0.001f, \
     .stroke_offset = 0.05,\
-    .chain_smooth_tolerance = 0.2f,\
+    .chain_smooth_tolerance = 0.0f,\
   }
 
 #define _DNA_DEFAULT_LengthGpencilModifierData \
