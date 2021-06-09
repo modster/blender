1f38c1f3b2e9ccae1d2ed4d288153d326d884209
Fabian Schempp <fabianschempp@googlemail.com>
2021-06-09 07:49:45 +0200

    - added offset and boundary data enums for nonmanifold mode.

--- source/blender/makesdna/DNA_node_types.h
+++ source/blender/makesdna/DNA_node_types.h
@@ -1240,6 +1240,9 @@ typedef struct NodeInputVector {
 typedef struct NodeGeometrySolidify{
   /* GeometryNodeAttributeInputMode */
   uint8_t mode;
+  uint8_t nonmanifold_offset_mode;
+  uint8_t nonmanifold_boundary_mode;
+  char _pad[5];
 } NodeGeometrySolidify;
 
 typedef struct NodeInputString {
