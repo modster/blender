b1d04825e0222803301304b913432c4af909eb11
soumya pochiraju <soumya.pochiraju@gmail.com>
2021-06-16 14:26:27 +0530

    No new features implemented, added comments to overlay/shaders/vector_vert.glsl

--- source/blender/draw/engines/overlay/shaders/vector_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/vector_vert.glsl
@@ -7,11 +7,11 @@ uniform vec3 colour;
 
 flat out vec4 finalColor;
 
-vec3 verts[5] = vec3[5](vec3(0.0, 0.0, 0.0),
-                        vec3(0.0, 0.0, 0.0),
-                        vec3(0.0, 0.0, 0.0),
-                        vec3(0.0, 0.0, 0.0),
-                        vec3(0.0, 0.0, 0.0));
+vec3 verts[5] = vec3[5](vec3(0.0, 0.0, 0.0),      //origin of arrow
+                        vec3(0.0, 0.0, 0.0),      //neck
+                        vec3(0.0, 0.0, 0.0),      //point on the right
+                        vec3(0.0, 0.0, 0.0),      //topmost point
+                        vec3(0.0, 0.0, 0.0));     //point on the left
 
 const int indices[6] = int[6](0,3,3,2,3,4);
 
@@ -22,14 +22,14 @@ void main() {
 
     vec3 pos = vec3(0.0,0.0,0.0);
     vec3 v = normalize(vector);
-    vec4 dir = ViewMatrixInverse * vec4(0.0,0.0,-1.0,0.0);
-    vec3 dir1 = normalize(cross(dir.xyz,v));
+    vec4 dir = ViewMatrixInverse * vec4(0.0,0.0,-1.0,0.0);     //viewing direction
+    vec3 dir1 = normalize(cross(dir.xyz,v));                   //perpendicular to view dir. and length of arrow
     if(gl_VertexID % 6 != 0){
 
-      verts[3] = v*length(vector)*scale + v*min_clamp;
-      verts[1] = v*length(vector)*scale + v*min_clamp - v*0.2;
-      verts[2] = verts[1] + dir1*0.0866;
-      verts[4] = verts[1] - dir1*0.0866;
+      verts[3] = v*length(vector)*scale + v*min_clamp;          //topmost point
+      verts[1] = v*length(vector)*scale + v*min_clamp - v*0.2;  //neck
+      verts[2] = verts[1] + dir1*0.0866;                        //right side point of head
+      verts[4] = verts[1] - dir1*0.0866;                        //left side point of head
     }
     pos += verts[indices[gl_VertexID % 6]];
     pos += objPosition;
