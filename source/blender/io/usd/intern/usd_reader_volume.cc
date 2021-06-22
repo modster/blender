4d1d7af43ccd44ce80380871d760ce9a4de11d65
makowalski <makowalski@nvidia.com>
2021-06-21 22:01:29 -0400

    USDVolumeReader improvements.
    Fixed Volume reference counting error (incorrect
    call to id_us_min()).  Fixed error reading fieldName
    attribute, which was incorrectly being read as a string
    rather than a token.  Avoiding use of auto and unneeded
    declararions.  Logically grouping related code.  Now
    invoking attribute Get() functions with the given time.

--- source/blender/io/usd/intern/usd_reader_volume.cc
+++ source/blender/io/usd/intern/usd_reader_volume.cc
@@ -52,12 +52,17 @@
 
 #include <iostream>
 
+namespace usdtokens {
+
+static const pxr::TfToken density("density", pxr::TfToken::Immortal);
+
+}
+
 namespace blender::io::usd {
 
 void USDVolumeReader::create_object(Main *bmain, const double /* motionSampleTime */)
 {
   Volume *volume = (Volume *)BKE_volume_add(bmain, name_.c_str());
-  id_us_min(&volume->id);
 
   object_ = BKE_object_add_only_object(bmain, OB_VOLUME, name_.c_str());
   object_->data = volume;
@@ -69,54 +74,59 @@ void USDVolumeReader::read_object_data(Main *bmain, const double motionSampleTim
     return;
   }
 
-  pxr::UsdVolVolume::FieldMap fields = volume_.GetFieldPaths();
-
-  std::string filepath;
-
   Volume *volume = static_cast<Volume *>(object_->data);
 
-  for (auto it = fields.begin(); it != fields.end(); ++it) {
+  if (!volume) {
+    return;
+  }
+
+  pxr::UsdVolVolume::FieldMap fields = volume_.GetFieldPaths();
+
+  for (pxr::UsdVolVolume::FieldMap::const_iterator it = fields.begin(); it != fields.end(); ++it) {
 
     pxr::UsdPrim fieldPrim = prim_.GetStage()->GetPrimAtPath(it->second);
 
-    if (fieldPrim.IsA<pxr::UsdVolOpenVDBAsset>()) {
-      pxr::UsdVolOpenVDBAsset fieldBase(fieldPrim);
+    if (!fieldPrim.IsA<pxr::UsdVolOpenVDBAsset>()) {
+      continue;
+    }
 
-      pxr::UsdAttribute filepathAttr = fieldBase.GetFilePathAttr();
-      pxr::UsdAttribute fieldNameAttr = fieldBase.GetFieldNameAttr();
+    pxr::UsdVolOpenVDBAsset fieldBase(fieldPrim);
 
-      std::string fieldName = "density";
+    pxr::UsdAttribute fieldNameAttr = fieldBase.GetFieldNameAttr();
 
-      if (fieldNameAttr.IsAuthored()) {
-        fieldNameAttr.Get(&fieldName, 0.0);
+    if (fieldNameAttr.IsAuthored()) {
+      pxr::TfToken fieldName;
+      fieldNameAttr.Get(&fieldName, motionSampleTime);
 
-        // A Blender volume creates density by default
-        if (fieldName != "density") {
-          BKE_volume_grid_add(volume, fieldName.c_str(), VOLUME_GRID_FLOAT);
-        }
+      /* A Blender volume creates density by default. */
+      if (fieldName != usdtokens::density) {
+        BKE_volume_grid_add(volume, fieldName.GetString().c_str(), VOLUME_GRID_FLOAT);
       }
+    }
 
-      if (filepathAttr.IsAuthored()) {
+    pxr::UsdAttribute filepathAttr = fieldBase.GetFilePathAttr();
 
-        pxr::SdfAssetPath fp;
-        filepathAttr.Get(&fp, 0.0);
+    if (filepathAttr.IsAuthored()) {
+      pxr::SdfAssetPath fp;
+      filepathAttr.Get(&fp, motionSampleTime);
 
-        if (filepathAttr.ValueMightBeTimeVarying()) {
-          std::vector<double> filePathTimes;
-          filepathAttr.GetTimeSamples(&filePathTimes);
+      if (filepathAttr.ValueMightBeTimeVarying()) {
+        std::vector<double> filePathTimes;
+        filepathAttr.GetTimeSamples(&filePathTimes);
 
-          int start = (int)filePathTimes.front();
-          int end = (int)filePathTimes.back();
+        if (!filePathTimes.empty()) {
+          int start = static_cast<int>(filePathTimes.front());
+          int end = static_cast<int>(filePathTimes.back());
 
-          volume->is_sequence = (char)true;
+          volume->is_sequence = static_cast<char>(true);
           volume->frame_start = start;
           volume->frame_duration = (end - start) + 1;
         }
-
-        filepath = fp.GetResolvedPath();
-
-        strcpy(volume->filepath, filepath.c_str());
       }
+
+      std::string filepath = fp.GetResolvedPath();
+
+      strcpy(volume->filepath, filepath.c_str());
     }
   }
 
