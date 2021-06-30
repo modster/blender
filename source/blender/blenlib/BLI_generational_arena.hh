ea058d61a4548c10d60532c34d2e4150a369da7d
ishbosamiya <ishbosamiya@gmail.com>
2021-06-30 10:30:15 +0530

    bli: generational_arena: add references

--- source/blender/blenlib/BLI_generational_arena.hh
+++ source/blender/blenlib/BLI_generational_arena.hh
@@ -52,6 +52,17 @@
  */
 /* TODO(ish): need to complete documentation */
 
+/**********************************************************************
+ * references
+ *
+ * RustConf 2018 - Closing Keynote - Using Rust For Game Development
+ * by Catherine West
+ * https://www.youtube.com/watch?v=aKLntZcp27M
+ *
+ * Rust crate `generational_arena`
+ * https://docs.rs/generational-arena/
+ *********************************************************************/
+
 #include <cstddef>
 #include <functional>
 #include <iterator>
