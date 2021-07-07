c400f79582b53350ab4f7a714c2584fae84912f9
Himanshi Kalra <himanshikalra98@gmail.com>
2021-07-07 19:09:50 +0530

    Small test file

--- tests/python/test_classes.py
+++ tests/python/test_classes.py
@@ -1,3 +1,4 @@
+
 import math
 import os
 import sys
@@ -6,10 +7,20 @@ from random import shuffle, seed
 import bpy
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from modules.base_mesh_test import SpecTest, BlendFileTest
-from modules.mesh_test import RunTest, ModifierSpec, MeshTest
+from modules.spec_classes import ModifierSpec, DeformModifierSpec, OperatorSpecEditMode, OperatorSpecObjectMode, ParticleSystemSpec
+from modules.base_mesh_test import SpecMeshTest, BlendFileTest
 
 # modifier_test = SpecTest("test_name_demo", "test_demo", "exp_demo", [])
 
-geo_node_test = BlendFileTest("test_object", "expected_object")
-geo_node_test.run_test()
+# geo_node_test = BlendFileTest("test_object", "expected_object")
+# geo_node_test.run_test()
+
+modifier_test = SpecMeshTest("CubeSubsurfPass", "test_object", "subsurf_obj",
+                             [ModifierSpec('subsurf', 'SUBSURF', {})])
+
+modifier_test.run_test()
+
+modifier_test_2 = SpecMeshTest("CubeSubsurffail", "test_object", "no_subsurf",
+                             [ModifierSpec('subsurf', 'SUBSURF', {})])
+
+modifier_test_2.run_test()
