f690cd36b549d2bc3aea29a08656980da09601d2
Manuel Castilla <manzanillawork@gmail.com>
2021-07-29 13:48:12 +0200

    Compositor: Full frame Hue Saturation Value node
    Adds full frame implementation to this node operation.
    No functional changes.

--- source/blender/compositor/operations/COM_ChangeHSVOperation.cc
+++ source/blender/compositor/operations/COM_ChangeHSVOperation.cc
@@ -71,4 +71,26 @@ void ChangeHSVOperation::executePixelSampled(float output[4],
   output[3] = inputColor1[3];
 }
 
+void ChangeHSVOperation::update_memory_buffer_partial(MemoryBuffer *output,
+                                                      const rcti &area,
+                                                      Span<MemoryBuffer *> inputs)
+{
+  for (BuffersIterator<float> it = output->iterate_with(inputs, area); !it.is_end(); ++it) {
+    const float *color = it.in(0);
+    const float hue = *it.in(1);
+    it.out[0] = color[0] + (hue - 0.5f);
+    if (it.out[0] > 1.0f) {
+      it.out[0] -= 1.0f;
+    }
+    else if (it.out[0] < 0.0f) {
+      it.out[0] += 1.0f;
+    }
+    const float saturation = *it.in(2);
+    const float value = *it.in(3);
+    it.out[1] = color[1] * saturation;
+    it.out[2] = color[2] * value;
+    it.out[3] = color[3];
+  }
+}
+
 }  // namespace blender::compositor
