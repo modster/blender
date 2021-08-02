bf5d1aa0f62a4440bf83935a734a738dad615b9e
makowalski <makowalski@nvidia.com>
2021-08-02 10:13:01 -0400

    USD importer: fixed typo
    Fixed typo in copyright block comments.

--- source/blender/io/usd/intern/usd_reader_mesh.h
+++ source/blender/io/usd/intern/usd_reader_mesh.h
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Adpated from the Blender Alembic importer implementation.
+ * Adapted from the Blender Alembic importer implementation.
  *
  * Modifications Copyright (C) 2021 Tangent Animation and
  * NVIDIA Corporation.  All rights reserved.
