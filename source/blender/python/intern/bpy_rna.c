1681d099194afb847de36794671b00c6f99ed028
Hans Goudey <h.goudey@me.com>
2021-08-05 13:57:35 -0500

    Put "\n" on newline

--- source/blender/python/intern/bpy_rna.c
+++ source/blender/python/intern/bpy_rna.c
@@ -4328,7 +4328,8 @@ static PyObject *pyrna_struct_id_properties_ensure(BPy_StructRNA *self)
 }
 
 PyDoc_STRVAR(pyrna_struct_id_properties_ui_doc,
-             ".. method:: id_properties_ui(key)\n\n"
+             ".. method:: id_properties_ui(key)\n"
+             "\n"
              "   :return: Return an object used to manage an IDProperty's UI data.\n"
              "   :arg key: String name of the property.\n"
              "   :rtype: :class:`bpy.types.IDPropertyUIManager`\n");
