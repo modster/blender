e30d5d183cc7afcdf95d65a42393adb1bdc7faf6
Manuel Castilla <manzanillawork@gmail.com>
2021-08-16 11:37:13 +0200

    Compositor: Full frame Map Value node

--- source/blender/compositor/operations/COM_MapValueOperation.cc
+++ source/blender/compositor/operations/COM_MapValueOperation.cc
@@ -60,4 +60,27 @@ void MapValueOperation::deinitExecution()
   this->m_inputOperation = nullptr;
 }
 
+void MapValueOperation::update_memory_buffer_partial(MemoryBuffer *output,
+                                                     const rcti &area,
+                                                     Span<MemoryBuffer *> inputs)
+{
+  for (BuffersIterator<float> it = output->iterate_with(inputs, area); !it.is_end(); ++it) {
+    const float input = *it.in(0);
+    TexMapping *texmap = this->m_settings;
+    float value = (input + texmap->loc[0]) * texmap->size[0];
+    if (texmap->flag & TEXMAP_CLIP_MIN) {
+      if (value < texmap->min[0]) {
+        value = texmap->min[0];
+      }
+    }
+    if (texmap->flag & TEXMAP_CLIP_MAX) {
+      if (value > texmap->max[0]) {
+        value = texmap->max[0];
+      }
+    }
+
+    it.out[0] = value;
+  }
+}
+
 }  // namespace blender::compositor
