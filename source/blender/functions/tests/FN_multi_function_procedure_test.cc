2101b46802fc1d50ed5bd1abdd7699a3502682a7
Jacques Lucke <jacques@blender.org>
2021-08-22 12:49:22 +0200

    fix comment

--- source/blender/functions/tests/FN_multi_function_procedure_test.cc
+++ source/blender/functions/tests/FN_multi_function_procedure_test.cc
@@ -15,6 +15,7 @@ TEST(multi_function_procedure, SimpleTest)
    * procedure(int var1, int var2, int *var4) {
    *   int var3 = var1 + var2;
    *   var4 = var2 + var3;
+   *   var4 += 10;
    * }
    */
 
