6ae20c4229f9c203b9a88f7e49e0439d1a57afbb
Jeroen Bakker <jeroen@blender.org>
2022-01-10 08:40:05 +0100

    gpu_shader_2D_point_uniform_size_uniform_color_outline_aa.

--- source/blender/gpu/shaders/infos/gpu_shader_2D_point_uniform_size_uniform_color_aa_info.hh
+++ source/blender/gpu/shaders/infos/gpu_shader_2D_point_uniform_size_uniform_color_aa_info.hh
@@ -1,11 +1,9 @@
 #include "gpu_interface_info.hh"
 #include "gpu_shader_create_info.hh"
 
-GPU_SHADER_INTERFACE_INFO(radii_iface, "").smooth(Type::VEC2, "radii");
-
 GPU_SHADER_CREATE_INFO(gpu_shader_2D_point_uniform_size_uniform_color_aa)
     .vertex_in(0, Type::VEC2, "pos")
-    .vertex_out(radii_iface)
+    .vertex_out(smooth_radii_iface)
     .fragment_out(0, Type::VEC4, "fragColor")
     .push_constant(0, Type::MAT4, "ModelViewProjectionMatrix")
     .push_constant(16, Type::VEC4, "color")
