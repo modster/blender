1eccb4fcbd93565a6349111be1d780160874d25e
Jeroen Bakker <jeroen@blender.org>
2022-01-14 15:09:31 +0100

    Updated license header.

--- source/blender/blenkernel/intern/lib_remap_test.cc
+++ source/blender/blenkernel/intern/lib_remap_test.cc
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * The Original Code is Copyright (C) 2020 by Blender Foundation.
+ * The Original Code is Copyright (C) 2022 by Blender Foundation.
  */
 #include "testing/testing.h"
 
