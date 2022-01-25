0f5044245d681df06879831a5b5436fde9dca972
Brecht Van Lommel <brecht@blender.org>
2022-01-25 13:25:33 +0100

    Cycles: add Point Info node
    With (center) position, radius and random value outputs.
    
    Eevee does not yet support rendering point clouds, but an untested
    implementation of this node was added for when it does.
    
    Ref T92573

--- release/scripts/startup/nodeitems_builtins.py
+++ release/scripts/startup/nodeitems_builtins.py
@@ -391,6 +391,7 @@ shader_node_categories = [
         NodeItem("ShaderNodeAmbientOcclusion"),
         NodeItem("ShaderNodeObjectInfo"),
         NodeItem("ShaderNodeHairInfo"),
+        NodeItem("ShaderNodePointInfo"),
         NodeItem("ShaderNodeVolumeInfo"),
         NodeItem("ShaderNodeParticleInfo"),
         NodeItem("ShaderNodeCameraData"),
