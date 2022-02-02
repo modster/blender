bcdbf76c57b4ac5808689759c2685a5e1e636465
Falk David <falkdavid@gmx.de>
2022-02-02 11:48:17 +0100

    Add comments to DNA for BKE_*_copy_settings functions

--- source/blender/makesdna/DNA_gpencil_types.h
+++ source/blender/makesdna/DNA_gpencil_types.h
@@ -326,6 +326,8 @@ typedef struct bGPDstroke {
   /** Curve used to edit the stroke using Bezier handlers. */
   struct bGPDcurve *editcurve;
 
+  /* NOTE: When adding new members, make sure to add them to BKE_gpencil_stroke_copy_settings as well! */
+
   bGPDstroke_Runtime runtime;
   void *_pad5;
 } bGPDstroke;
@@ -409,6 +411,8 @@ typedef struct bGPDframe {
   short flag;
   /** Keyframe type (eBezTriple_KeyframeType). */
   short key_type;
+
+  /* NOTE: When adding new members, make sure to add them to BKE_gpencil_frame_copy_settings as well! */
 
   bGPDframe_Runtime runtime;
 } bGPDframe;
@@ -532,6 +536,8 @@ typedef struct bGPDlayer {
   float location[3], rotation[3], scale[3];
   float layer_mat[4][4], layer_invmat[4][4];
   char _pad3[4];
+
+  /* NOTE: When adding new members, make sure to add them to BKE_gpencil_layer_copy_settings as well! */
 
   bGPDlayer_Runtime runtime;
 } bGPDlayer;
@@ -728,6 +734,8 @@ typedef struct bGPdata {
   int vertex_group_active_index;
 
   bGPgrid grid;
+
+  /* NOTE: When adding new members, make sure to add them to BKE_gpencil_data_copy_settings as well! */
 
   bGPdata_Runtime runtime;
 } bGPdata;
