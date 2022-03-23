f4dab644872862ad3f1c9623fcb87c638c4f3bab
Clément Foucault <foucault.clem@gmail.com>
2022-03-23 14:15:12 +0100

    EEVEE: Move Closure definitions to gpu_shader_codegen_lib.glsl
    This way they can be used as inputs to the closure eval functions.

--- source/blender/draw/engines/eevee/shaders/eevee_closure_lib.glsl
+++ source/blender/draw/engines/eevee/shaders/eevee_closure_lib.glsl
@@ -1,40 +1,4 @@
 
-struct ClosureDiffuse {
-  vec3 color;
-  vec3 N;
-  vec3 sss_radius;
-  uint sss_id;
-};
-
-struct ClosureReflection {
-  vec3 color;
-  vec3 N;
-  float roughness;
-};
-
-struct ClosureRefraction {
-  vec3 color;
-  vec3 N;
-  float roughness;
-  float ior;
-};
-
-struct ClosureVolume {
-  vec3 emission;
-  vec3 scattering;
-  vec3 transmittance;
-  float anisotropy;
-};
-
-struct ClosureEmission {
-  vec3 emission;
-};
-
-struct ClosureTransparency {
-  vec3 transmittance;
-  float holdout;
-};
-
 /* We use the weight tree pre-evaluation to weight the closures.
  * There is no need for the Closure type. */
 struct Closure {
