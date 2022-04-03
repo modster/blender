c6dc9c9c3318c455f70b6af680d028dcae0909d0
Clément Foucault <foucault.clem@gmail.com>
2022-04-03 14:05:34 +0200

    EEVEE: Fix camera node depth output

--- source/blender/gpu/shaders/material/gpu_shader_material_camera.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_camera.glsl
@@ -1,6 +1,6 @@
 void camera(out vec3 outview, out float outdepth, out float outdist)
 {
-  outdepth = abs(transform_point(g_data.P, ViewMatrix).z);
+  outdepth = abs(transform_point(ViewMatrix, g_data.P).z);
   outdist = distance(g_data.P, cameraPos);
   outview = normalize(g_data.P - cameraPos);
 }
