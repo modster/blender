815d9163eb2714a124fb7172461fa402552d8794
Clément Foucault <foucault.clem@gmail.com>
2022-04-05 00:41:28 +0200

    EEVEE: Fix object displacement node
    The previous refactor was not equivalent to the old implementation.

--- source/blender/gpu/shaders/material/gpu_shader_material_displacement.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_displacement.glsl
@@ -1,8 +1,9 @@
 void node_displacement_object(float height, float midlevel, float scale, vec3 N, out vec3 result)
 {
-  N = normal_world_to_object(N);
+  N = transform_direction(ModelMatrix, N);
   result = (height - midlevel) * scale * normalize(N);
-  result = normal_object_to_world(result);
+  /* Apply object scale and orientation. */
+  result = transform_direction(ModelMatrix, result);
 }
 
 void node_displacement_world(float height, float midlevel, float scale, vec3 N, out vec3 result)
