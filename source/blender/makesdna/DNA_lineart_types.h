f5cf49f893e3bfed65176daaa20f6f7d83a26322
YimingWu <xp8110@outlook.com>
2022-04-07 09:07:55 +0800

    LineArt: Face mark filtering for new object loading code.

--- source/blender/makesdna/DNA_lineart_types.h
+++ source/blender/makesdna/DNA_lineart_types.h
@@ -47,9 +47,16 @@ typedef enum eLineartEdgeFlag {
   LRT_EDGE_FLAG_MATERIAL = (1 << 3),
   LRT_EDGE_FLAG_INTERSECTION = (1 << 4),
   LRT_EDGE_FLAG_LOOSE = (1 << 5),
-  LRT_EDGE_FLAG_CHAIN_PICKED = (1 << 6),
-  LRT_EDGE_FLAG_CLIPPED = (1 << 7),
-  /** Limited to 8 bits, DON'T ADD ANYMORE until improvements on the data structure. */
+  /* LRT_EDGE_FLAG_FOR_FUTURE = (1 << 7), */
+  /* Limited to 8 bits for edge type flag, don't add anymore because BMEdge->head.eflag only has 8
+     bits. So unless we changed this into a non-single-bit flag thing, we keep it this way. */
+  /** Also used as discarded line mark. */
+  LRT_EDGE_FLAG_CHAIN_PICKED = (1 << 8),
+  LRT_EDGE_FLAG_CLIPPED = (1 << 9),
+  /** Limited to 16 bits for the entire thing. */
+
+  /** For object loading code to use only. */
+  LRT_EDGE_FLAG_INHIBIT = (1 << 14),
 } eLineartEdgeFlag;
 
 #define LRT_EDGE_FLAG_ALL_TYPE 0x3f
