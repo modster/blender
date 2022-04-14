a794d08844804f5d8b7b0cedd343af84c02e94f2
Clément Foucault <foucault.clem@gmail.com>
2022-04-14 15:22:58 +0200

    GPUMaterial: Fix vector displacement node

--- source/blender/gpu/shaders/material/gpu_shader_material_vector_displacement.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_vector_displacement.glsl
@@ -1,29 +1,22 @@
-void node_vector_displacement_tangent(vec3 vector,
-                                      float midlevel,
-                                      float scale,
-                                      vec4 tangent,
-                                      vec3 normal,
-                                      mat4 obmat,
-                                      mat4 viewmat,
-                                      out vec3 result)
+void node_vector_displacement_tangent(
+    vec4 vector, float midlevel, float scale, vec4 T, out vec3 result)
 {
-  vec3 oN = normalize(normal_world_to_object(normal));
-  vec3 oT = normalize(normal_world_to_object(tangent.xyz));
-  vec3 oB = tangent.w * normalize(cross(oN, oT));
+  vec3 oN = normalize(normal_world_to_object(g_data.N));
+  vec3 oT = normalize(normal_world_to_object(T.xyz));
+  vec3 oB = T.w * normalize(cross(oN, oT));
 
-  result = (vector - midlevel) * scale;
+  result = (vector.xyz - midlevel) * scale;
   result = result.x * oT + result.y * oN + result.z * oB;
   result = transform_point(ModelMatrix, result);
 }
 
-void node_vector_displacement_object(
-    vec3 vector, float midlevel, float scale, mat4 obmat, out vec3 result)
+void node_vector_displacement_object(vec4 vector, float midlevel, float scale, out vec3 result)
 {
-  result = (vector - midlevel) * scale;
+  result = (vector.xyz - midlevel) * scale;
   result = transform_point(ModelMatrix, result);
 }
 
-void node_vector_displacement_world(vec3 vector, float midlevel, float scale, out vec3 result)
+void node_vector_displacement_world(vec4 vector, float midlevel, float scale, out vec3 result)
 {
-  result = (vector - midlevel) * scale;
+  result = (vector.xyz - midlevel) * scale;
 }
