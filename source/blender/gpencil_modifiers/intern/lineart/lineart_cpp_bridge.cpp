19e0ba9800cb7d4d0da1b8a0493d878f4a46d622
YimingWu <xp8110@outlook.com>
2022-04-26 00:37:01 +0800

    LineArt: Properly working object loading with index

--- source/blender/gpencil_modifiers/intern/lineart/lineart_cpp_bridge.cpp
+++ source/blender/gpencil_modifiers/intern/lineart/lineart_cpp_bridge.cpp
@@ -3,26 +3,14 @@
 #include "MOD_lineart.h"
 #include "lineart_intern.h"
 
-static int cmp_adjacent_items(const LineartAdjacentItem &p1, const LineartAdjacentItem &p2)
+static bool cmp_adjacent_items(const LineartAdjacentItem &p1, const LineartAdjacentItem &p2)
 {
   int a = (int)p1.v1 - (int)p2.v1;
   int b = (int)p1.v2 - (int)p2.v2;
-  return a ? a : b;
+  return a < 0 ? true : (a == 0 ? b < 0 : false);
 }
 
 void lineart_sort_adjacent_items(LineartAdjacentItem *ai, int length)
 {
-  blender::Vector<LineartAdjacentItem> _ai;
-  _ai.reserve(length);
-  for (int i = 0; i < length; i++) {
-    _ai[i] = ai[i];
-    printf("(%d %d %d)", _ai[i].v1, _ai[i].v2, _ai[i].e);
-  }
-  _ai.resize(length);
-  printf("\n");
-  std::sort(ai, ai + length, cmp_adjacent_items);
-  for (int i = 0; i < length; i++) {
-    ai[i] = _ai[i];
-    printf("(%d %d %d)", _ai[i].v1, _ai[i].v2, _ai[i].e);
-  }
+  blender::parallel_sort(ai, ai + length - 1, cmp_adjacent_items);
 }
