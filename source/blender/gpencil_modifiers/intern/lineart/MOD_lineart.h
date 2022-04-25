a3b38c979aab0d355b02533bbac47fb80ec14bd8
YimingWu <xp8110@outlook.com>
2022-04-25 12:49:25 +0800

    Revert "LineArt: CAS-inspired tile tree (WIP)"
    This reverts commit 77afe859a0fecb3b03b9879a4d7419f991dc9f81.

--- source/blender/gpencil_modifiers/intern/lineart/MOD_lineart.h
+++ source/blender/gpencil_modifiers/intern/lineart/MOD_lineart.h
@@ -222,11 +222,8 @@ typedef struct LineartRenderBuffer {
 
   float overscan;
 
-  struct LineartBoundingArea **initial_bounding_areas;
-  unsigned int bounding_area_initial_count;
-
-  /* Array of thread_count length for spatial locks. */
-  SpinLock *lock_bounding_areas;
+  struct LineartBoundingArea *initial_bounding_areas;
+  unsigned int bounding_area_count;
 
   /* When splitting bounding areas, if there's an ortho camera placed at a straight angle, there
    * will be a lot of triangles aligned in line which can not be separated by continue subdividing
@@ -443,7 +440,7 @@ typedef struct LineartBoundingArea {
   double cx, cy;
 
   /** 1,2,3,4 quadrant */
-  struct LineartBoundingArea *child[4];
+  struct LineartBoundingArea *child;
 
   ListBase lp;
   ListBase rp;
