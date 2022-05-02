394b107aeff75ee4b12c03bd63a6c70712ce7440
Falk David <falkdavid@gmx.de>
2022-05-02 12:51:20 +0200

    Move DNA structs to separate file

---
+++ source/blender/blenkernel/intern/gpencil_new_proposal.hh
@@ -0,0 +1,100 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup DNA
+ */
+
+#pragma once
+
+#include "DNA_ID.h"
+#include "DNA_curves_types.h"
+#include "DNA_customdata_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Note: This should be in a file like BKE_gpencil.hh */
+namespace blender::bke::gpencil {
+class GPDataRuntime {
+ public:
+  /* Runtime Data */
+  /* void *stroke_painting_buffer; */
+};
+}  // namespace blender::bke::gpencil
+
+#ifdef __cplusplus
+class GPDataRuntime;
+using GPDataRuntimeHandle = blender::bke::gpencil::GPDataRuntime;
+#else
+typedef struct GPDataRuntimeHandle GPDataRuntimeHandle;
+#endif
+
+typedef struct GPLayerGroup {
+  struct GPLayerGroup *children;
+  int children_size;
+
+  int *layer_indices;
+  int layer_indices_size;
+
+  char name[128];
+
+  /* ... */
+} GPLayerGroup;
+
+typedef struct GPLayer {
+  char name[128];
+
+  int flag;
+
+  /* ... */
+} GPLayer;
+
+typedef struct GPFrame {
+  CurvesGeometry strokes;
+
+  int flag;
+
+  int start;
+  int end;
+
+  /* ... */
+} GPFrame;
+
+typedef struct GPData {
+  GPFrame *frames;
+  int frames_size;
+  CustomData frame_data;
+  int active_frame_index;
+
+  GPLayer *layers;
+  int layers_size;
+  int active_layer_index;
+
+  GPLayerGroup default_group;
+
+  GPDataRuntimeHandle *runtime;
+} GPData;
+
+/* This is the ID_GP structure that holds all the */
+typedef struct GreasePencil {
+  ID id;
+  /* Animation data (must be immediately after id). */
+  struct AnimData *adt;
+
+  /* Pointer to the actual data-block containing the frames, layers and layer groups. */
+  GPData *grease_pencil_data;
+
+  int flag;
+
+  /** Materials array. */
+  struct Material **mat;
+  /** Total materials. */
+  short totcol;
+
+  /* ... */
+} GreasePencil;
+
+#ifdef __cplusplus
+}
+#endif
--- source/blender/blenkernel/intern/gpencil_new_proposal_test.cc
+++ source/blender/blenkernel/intern/gpencil_new_proposal_test.cc
@@ -3,12 +3,13 @@
 /** \file
  * \ingroup bke
  */
-
 #include "BKE_curves.hh"
+#include "BLI_math_vec_types.hh"
 
+#include "gpencil_new_proposal.hh"
 #include "testing/testing.h"
 
-namespace blender::bke::gpencil {
+namespace blender::bke {
 
 class GPencilFrame : public CurvesGeometry {
  public:
@@ -27,85 +28,18 @@ class GPencilFrame : public CurvesGeometry {
   }
 };
 
-class GPDataRuntime {
+class GPData : ::GPData {
  public:
-  /* Runtime Data */
-  /* void *stroke_painting_buffer; */
+  GPData();
+  ~GPData();
 };
 
-class GPencilData : blender::dna::gpencil::GPData {
- public:
-  GPencilData();
-  ~GPencilData();
-};
-
-}  // namespace blender::bke::gpencil
-
-namespace blender::dna::gpencil {
-
-#ifdef __cplusplus
-class GPDataRuntime;
-using GPDataRuntimeHandle = blender::bke::gpencil::GPDataRuntime;
-#else
-typedef struct GPDataRuntimeHandle GPDataRuntimeHandle;
-#endif
-
-typedef struct GPLayerGroup {
-  struct GPLayerGroup *children;
-  int children_size;
-
-  int *layer_indices;
-  int layer_indices_size;
-
-  char name[128];
-} GPLayerGroup;
-
-typedef struct GPLayer {
-  char name[128];
-  int flag;
-} GPLayer;
-
-typedef struct GPData {
-  CurvesGeometry *frames;
-  int frames_size;
-  CustomData frame_data;
-  int active_frame_index;
-
-  GPLayer *layers;
-  int layers_size;
-  CustomData layer_data;
-  int active_layer_index;
-
-  GPLayerGroup default_group;
-
-  GPDataRuntimeHandle *runtime;
-} GPData;
-
-typedef struct GreasePencil {
-  ID id;
-  /* Animation data (must be immediately after id). */
-  struct AnimData *adt;
-
-  /**/
-  GPData grease_pencil_data;
-
-  int flag;
-
-  /** Materials array. */
-  struct Material **mat;
-  /** Total materials. */
-  short totcol;
-
-  /* ... */
-} GreasePencil;
-
-}  // namespace blender::dna::gpencil
+}  // namespace blender::bke
 
 namespace blender::bke::gpencil::tests {
 
 TEST(gpencil_proposal, Foo)
 {
-  using namespace blender::bke::gpencil;
   GPencilFrame my_frame;
   float3 min, max;
   EXPECT_FALSE(my_frame.bounds_min_max(min, max));
