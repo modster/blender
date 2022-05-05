5788f6cc37a9277032ae919741652784ee11018d
Falk David <falkdavid@gmx.de>
2022-05-05 11:07:17 +0200

    Implement adding frames to layers, add comments

--- source/blender/blenkernel/intern/gpencil_new_proposal_test.cc
+++ source/blender/blenkernel/intern/gpencil_new_proposal_test.cc
@@ -15,6 +15,7 @@ class GPLayerGroup : ::GPLayerGroup {
  public:
   GPLayerGroup()
   {
+    /* TODO */
   }
 
   ~GPLayerGroup()
@@ -31,7 +32,7 @@ class GPLayerGroup : ::GPLayerGroup {
 
 class GPDataRuntime {
  public:
-  mutable void *sbuffer;
+  /* mutable void *sbuffer */;
 };
 
 class GPLayer : public ::GPLayer {
@@ -46,6 +47,27 @@ class GPLayer : public ::GPLayer {
   }
 
   ~GPLayer() = default;
+
+  bool operator==(const GPLayer &other) const
+  {
+    return STREQ(this->name, other.name);
+  }
+};
+
+class GPFrame : public ::GPFrame {
+ public:
+  GPFrame()
+  {
+    this->layer_index = this->start = this->end = -1;
+  }
+
+  GPFrame(int layer_index)
+  {
+    this->layer_index = layer_index;
+    this->start = this->end = -1;
+  }
+
+  ~GPFrame() = default;
 };
 
 class GPData : public ::GPData {
@@ -65,11 +87,9 @@ class GPData : public ::GPData {
     if (this->frames_size > 0) {
       this->frames_array = reinterpret_cast<::GPFrame *>(
           MEM_calloc_arrayN(this->frames_size, sizeof(::GPFrame), __func__));
-      this->active_frame_index = 0;
     }
     else {
       this->frames_array = nullptr;
-      this->active_frame_index = -1;
     }
     CustomData_reset(&this->frame_data);
 
@@ -103,6 +123,16 @@ class GPData : public ::GPData {
     this->runtime = nullptr;
   }
 
+  Span<GPFrame> frames() const
+  {
+    return {(const GPFrame *)this->frames_array, this->frames_size};
+  }
+
+  MutableSpan<GPFrame> frames_for_write()
+  {
+    return {(GPFrame *)this->frames_array, this->frames_size};
+  }
+
   Span<GPLayer> layers() const
   {
     return {(const GPLayer *)this->layers_array, this->layers_size};
@@ -121,8 +151,26 @@ class GPData : public ::GPData {
     }
 
     // Move new_layer to the end in the array.
-    this->layers_array[this->layers_size - 1] = new_layer;
+    this->layers_for_write().last() = new_layer;
     return true;
+  }
+
+  const GPFrame &new_frame_on_layer(const int layer_index)
+  {
+    BLI_assert(layer_index >= 0 && layer_index < this->layers_size);
+
+    GPFrame new_frame(layer_index);
+    ensure_frame_array_has_size_at_least(this->frames_size + 1);
+    this->frames_for_write().last() = new_frame;
+
+    return this->frames().last();
+  }
+
+  const GPFrame &new_frame_on_layer(GPLayer &layer)
+  {
+    int index = this->layers().first_index_try(layer);
+    BLI_assert(index != -1);
+    return new_frame_on_layer(index);
   }
 
  private:
@@ -147,25 +195,33 @@ class GPData : public ::GPData {
 
     return true;
   }
+
+  const bool ensure_frame_array_has_size_at_least(int64_t size)
+  {
+    if (this->frames_size > size) {
+      return true;
+    }
+
+    int old_size = this->frames_size;
+    this->frames_size = size;
+
+    ::GPFrame *new_array = reinterpret_cast<::GPFrame *>(
+        MEM_calloc_arrayN(this->frames_size, sizeof(::GPFrame), __func__));
+    if (new_array == nullptr) {
+      return false;
+    }
+
+    memcpy(new_array, this->frames_array, old_size * sizeof(::GPFrame));
+    MEM_SAFE_FREE(this->frames_array);
+    this->frames_array = new_array;
+
+    return true;
+  }
 };
 
 }  // namespace blender::bke
 
 namespace blender::bke::gpencil::tests {
-
-TEST(gpencil_proposal, Foo)
-{
-  //   GPData my_data;
-  //   GPLayer my_layer("FooLayer");
-
-  //   my_data.add_layer(my_layer);
-  //   GPFrame my_frame = my_data.new_frame_on_layer(my_layer);
-  //   my_frame.set_start_and_end(5, 10);
-
-  //   GPStroke my_stroke(100);
-  //   fill_stroke_with_points(my_stroke);
-  //   my_frame.insert_stroke(my_stroke);
-}
 
 TEST(gpencil_proposal, EmptyGPData)
 {
@@ -228,6 +284,18 @@ TEST(gpencil_proposal, ChangeLayerName)
 
   EXPECT_EQ(my_data.layers_size, 1);
   EXPECT_STREQ(my_data.layers().last().name, "BarLayer");
+}
+
+TEST(gpencil_proposal, AddFrameToLayer)
+{
+  GPData my_data;
+  GPLayer my_layer1("TestLayer1");
+  GPLayer my_layer2("TestLayer2");
+
+  my_data.add_layer(my_layer1);
+  my_data.add_layer(my_layer2);
+  GPFrame my_frame = my_data.new_frame_on_layer(my_layer2);
+  EXPECT_EQ(my_frame.layer_index, 1);
 }
 
 }  // namespace blender::bke::gpencil::tests
