0825869c82d89f1f4ce6711735abf9245053deef
Jeroen Bakker <jeroen@blender.org>
2022-05-06 16:33:40 +0200

    Store metadata in image buffer.

--- source/blender/compositor/operations/COM_ViewerOperation.cc
+++ source/blender/compositor/operations/COM_ViewerOperation.cc
@@ -202,6 +202,9 @@ void ViewerOperation::update_image(const rcti *rect)
                                     rect->ymax);
 
   std::unique_ptr<MetaData> metadata = image_input_->get_meta_data();
+  if (metadata.has_value()) {
+    // TODO:    metadata.add_to_id_prop.
+  }
 
   /* This could be improved to use partial updates. For now disabled as the full frame compositor
    * would not use partial frames anymore and the image engine requires more testing. */
