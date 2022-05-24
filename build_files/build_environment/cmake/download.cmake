e625d0d74941f91771b13c3478f8f481e199a9a0
Ray Molenkamp <github@lazydodo.com>
2022-05-24 13:29:02 -0600

    deps_builder: fix typo in download.cmake

--- build_files/build_environment/cmake/download.cmake
+++ build_files/build_environment/cmake/download.cmake
@@ -114,6 +114,6 @@ download_source(IGC_VCINTRINSICS)
 download_source(IGC_SPIRV_HEADERS)
 download_source(IGC_SPIRV_TOOLS)
 download_source(IGC_SPIRV_TRANSLATOR)
-download_source(INTEL_GMMLIB)
+download_source(GMMLIB)
 download_source(OCLOC)
 
